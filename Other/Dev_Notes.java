19ddad6390f067372ce5fecde4660500d1710cc5
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2022-09-30 09:08:26 -0400

    Update Dev_Notes.java

--- Other/Dev_Notes.java
+++ Other/Dev_Notes.java
@@ -8,15 +8,19 @@ public class Dev_Notes
      * change skip turn criteria
      * fix evade
      * fix shatter
+     * make ws abilities
+     * ws needs a passive
+     * test sl and ws before rolling out
+     * BEFORE DOING FURY OVERRIDE SUMMON METHODS
      * 
      * Before adding any new content:
      * Add status effects to the game (and check for their interaction methods, e.g. characters cannot evade while submerged so change evade criteria)
-     * When adding passives, make sure Summons already have that method overloaded so there isn't switch confusion
      * Be careful about typos (e.g. !(contains) instead of contains); re read everything
      * Remember that characters' turn orders are set to 616 at death; on rez, they need to be reset     
      * Make sure to add new status effects to the factory first
      * Evade method justevaded binary eff should be cleared on turnend
      * When making changes to status effects, make the change to its Other counterpart too (e.g. change ShatterE when changing Shatter)
+     * When making changes to character methods (especially with passives), make the change to the corresponding overriden Summon methods too
      * 
      * Known issues   
      * SHATTER MUST IGNORE EVADE IF SUCCESSFULLY APPLIED
@@ -25,8 +29,11 @@ public class Dev_Notes
      * 
      * Balance changes
      * reduce cd of im boost ?
+     * make sl wound 1 turn (was 2)
      * 
-     * Edited in BlueJ (i.e. need to update in here):
-     * 
+     * Edited (i.e. need to update):
+     * debuffeff (countdown)
+     * stateff (checkapply, checkstacking, applyfail, statfactory)
+     * ability (getranomdenemy, ablistplayer)
      * */
 } 
