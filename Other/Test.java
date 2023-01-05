ea7a69c92736e763c2979c7e10c4041f74c5f5d3
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2023-01-05 11:48:58 -0500

    Update Test.java

--- Other/Test.java
+++ Other/Test.java
@@ -1,9 +1,32 @@
 public class Test 
 {
-   public static void main (String[] args)
-   {
-       
-   }
+   public static Ability MakeAbDrax (int counter) 
+    {
+        String[] p= new String[5];
+        switch (counter)
+        {
+            case 0: BasicAb headbutt =new BasicAb ("Headbutt", "single", "enemy", 45); 
+            cdart.AddStatString(despair); 
+            return headbutt; 
+            case 1: AttackAb twins= new AttackAb("Twins Blades", "single", "enemy", 80, 3); 
+            String[] despair=StatFactory.SetParam("Bleed", "100", "5", "2", "false");
+            String[] knock=StatFactory.SetParam("Bleed", "100", "5", "2", "false"); 
+            twins.AddStatString(despair); twins.AddStatString(knock); twins.together=false;
+            String[] life= StatFactory.SetParam("Drain", "500", "616", "2", "true", "Half"); twins.AddStatString(life);
+            return twins; 
+            case 2: DefAb fierce= new DefAb ("Fierce Protector", "single", "Ally exclusive", 3); String[] gloom=StatFactory.SetParam ("Protect", "500", "1", "616", "true");
+            String[] one=StatFactory.SetParam("Counter", chance, "35", "2", "true"); 
+            String[] two=StatFactory.SetParam("Counter", chance, "35", "2", "true"); 
+            fierce.AddStatString(gloom); fierce.AddStatString(one); fierce.AddStatString(two);
+            return fierce; 
+            case 3: AttackAb dice= new AttackAb("Slice and Dice", "single", "enemy", 80, 3); 
+            return dice; 
+            case 4: AttackAb Khonshu= new AttackAb ("Fist of Khonshu", "single", "enemy", 110, 4); String[] loss=StatFactory.SetParam("Stun", "100", "616", "616", "false");
+            Khonshu.AddStatString (loss); 
+            return Khonshu;
+        }
+        return null;
+    }    
    
    Speed (Character fast)
    {
@@ -25,4 +48,5 @@ public class Test
          } //end if
       } //end loop
    } //end method
+   
 } //end class
