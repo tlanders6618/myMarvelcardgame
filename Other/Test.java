c97bc242f98d5c753e54911ef086edfee54dfaee
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2023-01-05 09:51:40 -0500

    Update Test.java

--- Other/Test.java
+++ Other/Test.java
@@ -4,4 +4,21 @@ public class Test
    {
        
    }
-}
+   
+   Speed (Character fast)
+   {
+      //check fast's team
+      for (int i, etc)
+      {
+         if (team1[i]==fast)
+         {
+            //all code is in here
+            if (i!=0)
+            {
+              Character bobby=team[i-1];
+            }
+            
+         } //end if
+      } //end loop
+   } //end method
+} //end class
