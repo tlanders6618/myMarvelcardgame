4d911f6e00af0b54d8c00597e10321af0454f217
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2023-02-08 12:31:56 -0500

    Update Test.java

--- Other/Test.java
+++ Other/Test.java
@@ -3,40 +3,7 @@ public class Test
 all buff abilities should have a cooldown
 target filter doesn't account for 2 enemies being banished at same time
   
-    }    
-   public static Ability MakeAbX23 (int counter) 
-     {
-        switch (counter)
-        {
-            case 0: BasicAb karate= new BasicAb ("Karate Kick", "single", "enemy", 45);
-            return karate; 
-            case 1: BasicAb slash= new BasicAb ("Precision Slash", "single", "enemy", 35); String[] sun=StatFactory.SetParam("Precision", "100", "616", "2", "true");
-            slash.AddStatString(sun);
-            return slash; //make precision and counter and check shatter for accuracy
-            case 2: return null;
-            case 3: AttackAb out= new AttackAb("Bleed Them Out", "single", "enemy", 60, 3); String[] bled=StatFactory.SetParam("Bleed", "100", "20", "2", "false");
-            out.AddStatString(bled);
-            return out; 
-            case 4: AttackAb trip= new AttackAb ("Triple Slash", "single", "enemy", 30, 3); trip.multi=2; trip.special.add(new Multichain(trip.multi) );
-            return trip;
-        }
-        return null;
-    }  
-   public static Ability MakeAbWolvie (int counter) 
-    {
-        switch (counter)
-        {
-            case 0: BasicAb x= new BasicAb("X-Slash", "single", "enemy", 35); String[] julie=StatFactory.SetParam("Bleed", "50", "15", "1", "false");
-            return x; 
-            case 1: BasicAb primal= new BasicAb ("Primal Punch", "single", "enemy", 45); 
-            return primal; 
-            case 2: return null; 
-            case 3: return null; 
-            case 4: AttackAb best= new AttackAb ("Best There Is", "single", "enemy", 100, 5); best.special.add(new DamageCounterRemove (all but other on self));
-            return best;
-        }
-        return null;
-    }  
+    }   
   
    Speed (Character fast)
    {
