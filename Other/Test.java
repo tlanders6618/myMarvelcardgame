afafcf48721c1d3acf7a696c6200c02cfa9d0cb9
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2023-03-14 12:15:40 -0400

    Update Test.java

--- Other/Test.java
+++ Other/Test.java
@@ -2,30 +2,20 @@ public class Test
 {
 all buff abilities should have a cooldown
 target filter doesn't account for 2 enemies being banished at same time
-
-   
-    
-   Speed (Character fast)
+  
+   Speed (Character fast) //consider if two people have speed and if everyone has speed, nothing should happen; effectively no one has it
    {
       //check fast's team
-      for (int i, etc)
+      for (loop)
       {
-         if (team1[i]==fast)
+         if (the last character on this team who took a turn isn't fast)
          {
-            //all code is in here
-            make sure to check this code works both when speed is being applied on fast's turn and when it's being applied off their turn
-            if (i!=0) //the character with speed is not in the first position of the array
-            {
-              Character C11=team[i-1]; //one in front of fast
-              Character c13= //the one going right after fast
-            }
-            else if (i==0) //the one being snared is already going first but isn't necessarily taking their turn now
-            {
-               Character C12=team1[1]; Character C13=team1[2]; //team1[0]=fast already
-            }
-            
+            fast is the active character
+         } 
+         else if (it was fast)
+         {
+            ++p1active to skip fast
          } //end if
       } //end for loop
-   } //end method
-   
+   } //end method   
 } //end test class
