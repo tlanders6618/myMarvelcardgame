7fb4e8c0422e7b172f7975a0efdc7b66fc5b6ec4
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2024-02-25 18:08:33 -0500

    Add files via upload

--- Other/Dev_Notes.java
+++ Other/Dev_Notes.java
@@ -2,50 +2,104 @@ package myMarvelcardgamepack;
 public class Dev_Notes
 {
     /** 
-     * un-uncomment the thing in battle ALWAYS
+     * ***FILES***
+     * Abilities:
+     * -Ability
+     * -AbilityListPlayer
+     * -AbilityListSummon
+     * -AfterAbility
+     * -AttackAb
+     * -BasicAb
+     * -BeforeAbility
+     * -BuffAb
+     * -DebuffAb
+     * -DefenceAb
+     * -HealAb
+     * -OtherAb
+     * -SpecialAbility
+     * 
+     * Characters:
+     * -ActivePassive
+     * -Character
+     * -Hero
+     * -StaticPassive
+     * -Summon
+     * -SummonPassive
+     * 
+     * Combat:
+     * -Battle 
+     * -CardGameMain
+     * -CardHashcode
+     * -CardSelection
+     * -CoinFlip
+     * -DamageStuff
+     * -Scoreboard
+     * 
+     * Other:
+     * -DevNotes
+     * -Test
+     * 
+     * Status Effects:
+     * -BuffEff
+     * -DebuffEff
+     * -DefEff
+     * -HealEff
+     * -OtherEff
+     * -StatEff
+     * -StatFactory
+     * 
+     * 
+     * all new methods go under cardhashcode which is empty for some reason
+     * 
+     * Snared 3rd in turn order gets moved to first and everyone else moves down one in array
+     * daredevil blindside empowerment has to add an obj to beforeab that makes the ab miss; cannot add missed without letting beforeab make char immune to miss
      * 
      * To Do
-     * BANISH MUST --P1HEROES WHEN APPLIED
-     * make boolean aoe and boolean attack attributes of all abs by default
-     * check for blind before doing beforeabs and make damage counter not trigger on blind
-     * make tracker stateff
-     * change empowerments to work with aoe across the board
-     * change skip turn criteria
-     * make shatter method
-     * merge attack ignore shield with normal attack method
+     * fix turn skip criteria; think about elixir and chars with no attack abs
      * add altname for stateff and check for immunity to it when applying stateffs
-     * add alttype (damaging vs not) for stateff
      * double check glossary desc before doing assist and multitarget
-     * card selection doesnt distinguish between taunt def and taunt other
+     * card selection doesnt distinguish between taunt def and taunt other (tauntE doesnt exist)
+     * change counter and reflect to work with attacked method instead of manually checking for them
+     * change untargetable to check for binaries instead of having no counter
+     * rewrite lose hp attack option
+     * attack method no longer trigger onattacked for aoe abs; attackab must trigger it if ab is aoe
      * 
-     * Before adding any new content:
+     * Keep in mind:
      * Add status effects to the game (and check for their interaction methods, e.g. characters cannot evade while submerged so change evade criteria)
-     * Be careful about typos (e.g. !(contains) instead of contains); re read everything
-     * Remember that characters' turn orders are set to 616 at death; on rez, they need to be reset     
-     * Make sure to add new status effects to the factory  
      * When making changes to status effects, make the change to its Other counterpart too (e.g. change ShatterE when changing Shatter)
-     * When making changes to character methods make the change to the corresponding overriden Summon methods too
      * Sourceless damage (e.g. reflection) must print the took damage statement on their own; tookdamage will not
      * 
      * Known issues   
-     * SHATTER ATTACKS MUST IGNORE EVADE IF SUCCESSFULLY APPLIED but in lower case (make method)
-     * lunar protector doesn't trigger if the protected target is killed by the attack
+     * using counter causes null exception, most likely due to checking for a stateff to apply and finding none
+     * x23 multichain doesn't do third hit or keeps going until 3 kills or hits two different enemies
+     * something wrong with x23 crit calc
      * 
      * Balance changes:
+     * x23 regen strength
+     * berserker frenzy and gamora and modern fury kill mode trigger while stunned update passive desc 
+     * 
      * 
      * Edited (i.e. need to update):
+     * EVERYTHING
      * 
      * to test:
      * counter attack must trigger the took damage method for the attacker, with boolean true
+     * does tracker display, update properly?
+     * berserker rage triggers once
+     * x23 crit chance triggers once
+     * drain works
      * 
-     * Think about vapor and gambit ctd
+     * Always add ricochet before multichain in an ability's afterabs
+     * always add ignore miss as an ability's first beforeab
      * 
      * Preparation for checkfor -> check binaries conversion
      * ability
      * character
+     * hero
+     * summon
      * card selection
      * stateff
-     * battle (banish)
+     * battle
      * 
      * */
 } 
--- Other/Test.java
+++ Other/Test.java
@@ -1,21 +1,48 @@
+package myMarvelcardgamepack;
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
 public class Test 
 {
-all buff abilities should have a cooldown
-target filter doesn't account for 2 enemies being banished at same time
-  
-   Speed (Character fast) //consider if two people have speed and if everyone has speed, nothing should happen; effectively no one has it
+   public static void main (String[] args)
    {
-      //check fast's team
-      for (loop)
-      {
-         if (the last character on this team who took a turn isn't fast)
-         {
-            fast is the active character
-         } 
-         else if (it was fast)
-         {
-            ++p1active to skip fast
-         } //end if
-      } //end for loop
-   } //end method   
-} //end test class
+       //System.out.println(System.getProperty("java.class.version")); //55 means 11, 56 means 12, etc
+       //classic drax is 12, modern drax is 13, x23 is 14, wolvie is 15
+       Character Char11= new Hero (10); //Char11.add(Char11, new Intensify (0, 1500, 2)); //Char11.binaries.add("Stunned");
+       Character Char12= new Summon (7); Char12.binaries.add("Stunned");
+       Character Char13= new Summon (7); Char13.binaries.add("Stunned");
+       Character Char21= new Hero (8); Char21.add(Char21, new Intensify (0, 90, 2)); //Char21.binaries.add("Stunned"); 
+       Character Char22= new Hero (10); Char22.binaries.add("Stunned");
+       Character Char23= new Summon (7); Char23.binaries.add("Stunned");
+       
+       boolean Pwinner=false; 
+       Pwinner=Battle.main(Char11, Char12, Char13, Char21, Char22, Char23);
+       if (Pwinner==true)
+       {
+           System.out.println ("\nPlayer 1 wins!");
+       }
+       else if (Pwinner==false)
+       {
+           System.out.println ("\nPlayer 2 wins!");
+       }        
+       Card_Game_Main.trash.close();
+       /*
+        * To do/to test today:
+        * make empowerments work with aoe for both regular and channelled abs
+        * test shatter interact with protect
+        * test snare same way as speed
+        * test drax and drax
+        */
+       
+       /** 
+        for (int i = 0; i < n.length; i++) 
+       { //this is equal to the row 
+         for (int j = 0; j < n[i].length; j++) 
+         { //this is equal to the column in each row.
+            System.out.print(n[i][j] + " ");
+         }
+         System.out.println("\n");
+         //This prints the contents of a 2d array; save for later
+       }//*/
+   }
+}
