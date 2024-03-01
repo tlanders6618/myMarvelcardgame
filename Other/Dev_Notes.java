02aefa0ef3bb397ecf5d90913f3d2c5ff6b79c18
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2024-03-01 16:35:41 -0500

    Add files via upload

--- Other/Dev_Notes.java
+++ Other/Dev_Notes.java
@@ -2,6 +2,9 @@ package myMarvelcardgamepack;
 public class Dev_Notes
 {
     /** 
+     * Edited (i.e. need to update):
+     * EVERYTHING
+     * 
      * ***FILES***
      * Abilities:
      * -Ability
@@ -58,7 +61,6 @@ public class Dev_Notes
      * fix turn skip criteria; think about elixir and chars with no attack abs
      * add altname for stateff and check for immunity to it when applying stateffs
      * double check glossary desc before doing assist and multitarget
-     * card selection doesnt distinguish between taunt def and taunt other (tauntE doesnt exist)
      * change counter and reflect to work with attacked method instead of manually checking for them
      * change untargetable to check for binaries instead of having no counter
      * rewrite lose hp attack option
@@ -71,35 +73,13 @@ public class Dev_Notes
      * 
      * Known issues   
      * using counter causes null exception, most likely due to checking for a stateff to apply and finding none
-     * x23 multichain doesn't do third hit or keeps going until 3 kills or hits two different enemies
-     * something wrong with x23 crit calc
-     * 
-     * Balance changes:
-     * x23 regen strength
-     * berserker frenzy and gamora and modern fury kill mode trigger while stunned update passive desc 
-     * 
-     * 
-     * Edited (i.e. need to update):
-     * EVERYTHING
      * 
      * to test:
      * counter attack must trigger the took damage method for the attacker, with boolean true
-     * does tracker display, update properly?
-     * berserker rage triggers once
-     * x23 crit chance triggers once
      * drain works
      * 
      * Always add ricochet before multichain in an ability's afterabs
      * always add ignore miss as an ability's first beforeab
-     * 
-     * Preparation for checkfor -> check binaries conversion
-     * ability
-     * character
-     * hero
-     * summon
-     * card selection
-     * stateff
-     * battle
      * 
      * */
 } 
--- Other/Test.java
+++ Other/Test.java
@@ -7,16 +7,15 @@ public class Test
    public static void main (String[] args)
    {
        //System.out.println(System.getProperty("java.class.version")); //55 means 11, 56 means 12, etc
-       //classic drax is 12, modern drax is 13, x23 is 14, wolvie is 15
-       Character Char11= new Hero (10); //Char11.add(Char11, new Intensify (0, 1500, 2)); //Char11.binaries.add("Stunned");
-       Character Char12= new Summon (7); Char12.binaries.add("Stunned");
+       Character Char11= new Hero (16); //Char11.add(Char11, new Taunt (0, 90)); //Char11.binaries.add("Stunned");
+       Character Char12= new Hero (1); Char12.binaries.add("Stunned");
        Character Char13= new Summon (7); Char13.binaries.add("Stunned");
-       Character Char21= new Hero (8); Char21.add(Char21, new Intensify (0, 90, 2)); //Char21.binaries.add("Stunned"); 
-       Character Char22= new Hero (10); Char22.binaries.add("Stunned");
+       Character Char21= new Hero (3); //Char21.binaries.add("Stunned");  //Char21.add(Char21, new Countdown (100, 250, 1, null)); 
+       Character Char22= new Summon (12); Char22.binaries.add("Stunned"); 
        Character Char23= new Summon (7); Char23.binaries.add("Stunned");
-       
        boolean Pwinner=false; 
        Pwinner=Battle.main(Char11, Char12, Char13, Char21, Char22, Char23);
+       System.out.println("Donut forget to update card selection indexes");
        if (Pwinner==true)
        {
            System.out.println ("\nPlayer 1 wins!");
@@ -28,18 +27,16 @@ public class Test
        Card_Game_Main.trash.close();
        /*
         * To do/to test today:
-        * make empowerments work with aoe for both regular and channelled abs
+        * make empowerments work with aoe for both regular and channelled abs; change chain method too
         * test shatter interact with protect
-        * test snare same way as speed
-        * test drax and drax
         */
        
        /** 
-        for (int i = 0; i < n.length; i++) 
+        for (int r = 0; i < arr.length; r++) 
        { //this is equal to the row 
-         for (int j = 0; j < n[i].length; j++) 
+         for (int c = 0; c < arr[r].length; c++) 
          { //this is equal to the column in each row.
-            System.out.print(n[i][j] + " ");
+            System.out.print(arr[r][c] + " ");
          }
          System.out.println("\n");
          //This prints the contents of a 2d array; save for later
