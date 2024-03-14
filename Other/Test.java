4db198dbc281eae78a68054c90fb0935a46897d8
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2024-03-14 10:10:34 -0400

    Add files via upload

--- Other/Test.java
+++ Other/Test.java
@@ -7,13 +7,14 @@ public class Test
    public static void main (String[] args)
    {
        //System.out.println(System.getProperty("java.class.version")); //55 means 11, 56 means 12, etc
-       Character Char11= new Hero (23); Char11.HP=100; //Char11.CC=50; //Char11.binaries.add("Stunned"); 
-       Character Char12= new Hero (22); Char12.binaries.add("Stunned"); //Char12.add(Char12, new Invisible(66, 66));
+       Character Char11= new Hero (13); //Char11.binaries.add("Stunned"); 
+       Character Char12= new Hero (21); Char12.binaries.add("Stunned"); 
        Character Char13= new Summon (12); Char13.binaries.add("Stunned");
-       Character Char21= new Hero (9); //Char21.binaries.add("Stunned"); 
-       Character Char22= new Hero (6); Char22.binaries.add("Stunned"); 
-       Character Char23= new Summon (12); Char23.binaries.add("Stunned"); 
+       Character Char21= new Hero (11); //Char21.binaries.add("Stunned"); 
+       Character Char22= new Summon (12); Char22.binaries.add("Stunned"); 
+       Character Char23= new Hero (13); Char23.binaries.add("Stunned"); 
        boolean Pwinner=false; 
+       //Char22.add(Char11, new Evade (100)); Char22.add(Char11, new Taunt (100)); //Char12.add(Char11, new Intensify(66, 5, 66)); //Char22.add(Char11, new Burn(100, 0, 2)); 
        System.out.println("\nDonut forget to update card selection indexes");
        Pwinner=Battle.main(Char11, Char12, Char13, Char21, Char22, Char23);
        if (Pwinner==true)
@@ -27,9 +28,9 @@ public class Test
        Card_Game_Main.trash.close();
        /*
         * To do/to test today:
-        * make sure empowerments work as intended
         * do draft mode soon
-        * check how army of one works with evasion
+        * randomly taking bleed dmg twice (captain a took thrice; flash took twice)
+        * rewrite modok passive to account for barrier  
         */
        
        /** 
