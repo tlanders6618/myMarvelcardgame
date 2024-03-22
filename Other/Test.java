07648720a2ef6ebaef2c5692c4a4c846b18628d8
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2024-03-22 09:57:28 -0400

    Add files via upload

--- Other/Test.java
+++ Other/Test.java
@@ -7,14 +7,14 @@ public class Test
    public static void main (String[] args)
    {
        //System.out.println(System.getProperty("java.class.version")); //55 means 11, 56 means 12, etc
-       Character Char11= new Hero (13); //Char11.binaries.add("Stunned"); 
+       Character Char11= new Hero (18); //Char11.binaries.add("Stunned"); 
        Character Char12= new Hero (21); Char12.binaries.add("Stunned"); 
        Character Char13= new Summon (12); Char13.binaries.add("Stunned");
        Character Char21= new Hero (11); //Char21.binaries.add("Stunned"); 
        Character Char22= new Summon (12); Char22.binaries.add("Stunned"); 
        Character Char23= new Hero (13); Char23.binaries.add("Stunned"); 
        boolean Pwinner=false; 
-       //Char22.add(Char11, new Evade (100)); Char22.add(Char11, new Taunt (100)); //Char12.add(Char11, new Intensify(66, 5, 66)); //Char22.add(Char11, new Burn(100, 0, 2)); 
+       //Char21.add(new Neutralise(100, 4)); //Char11.add(new Bulwark (100, 2)); Char11.Cchance=-100; 
        System.out.println("\nDonut forget to update card selection indexes");
        Pwinner=Battle.main(Char11, Char12, Char13, Char21, Char22, Char23);
        if (Pwinner==true)
@@ -30,7 +30,7 @@ public class Test
         * To do/to test today:
         * do draft mode soon
         * randomly taking bleed dmg twice (captain a took thrice; flash took twice)
-        * rewrite modok passive to account for barrier  
+        * test that extend doesn't affect immortal/stun
         */
        
        /** 
