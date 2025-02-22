3640041ff1452b1cb1d9cce85de822220d4a89fb
tlanders6618 <113645988+tlanders6618@users.noreply.github.com>
2025-02-21 22:14:58 -0500

    Update Ability_List_Summon.java

--- Abilities/Ability_List_Summon.java
+++ Abilities/Ability_List_Summon.java
@@ -1,14 +1,21 @@
 package myMarvelcardgamepack;
 
-
 /**
- * Designer: Timothy Landers
- * Date: 7/8/22
- * Filename: Ability_List_Summon
- * Purpose: Lists all summon abilities.
+ * @author Timothy Landers
+ * Date of creation: 7/8/22
+ * Purpose: Creates/lists all summon abilities.
  */
 public class Ability_List_Summon
 {
+    /**
+    * Returns the given ability for the given Summon. Whether the ability is being copied or not must be specified.
+    * <p> This is because copied abilities are made differently, due to the implentation of some heroes' abilities being intertwined with their passives.
+    * @param index The index of the Summon whose abilities are being retrieved.
+    * @param counter The number of the ability being retrieved, e.g. the Summon's 4th ability. The numbering is identical to the order abilities are listed on their cards.
+    * @param copy Whether the ability is being made for the original Summon or being copied (e.g. by Rogue).
+    * @return The requested ability.
+    * @see Ability
+    */
     public static Ability GetAb (int index, int counter, boolean copy) //copy is for characters like rogue
     {
         //attack abs construction: String name, String type, String friendly, int dmg, int cooldown
@@ -35,7 +42,7 @@ public class Ability_List_Summon
         }
         return null;
     }    
-    public static Ability MakeAbName (int counter)
+    private static Ability MakeAbName (int counter)
     {
         switch (counter)
         {
@@ -47,12 +54,11 @@ public class Ability_List_Summon
             default: return null; 
         }
     }
-    public static Ability MakeAbSquid (int counter)
+    private static Ability MakeAbSquid (int counter)
     {
         switch (counter)
         {
-            case 0: BasicAb rend= new BasicAb("Tentacle Whip", "single", "enemy", 35);
-            return rend;
+            case 0: return new BasicAb("Tentacle Whip", "single", "enemy", 35);
             case 2: AttackAb r= new AttackAb("Ink Spray", "single", "enemy", 70, 4);
             String[] truth={"Blind", "100", "616", "1", "false"}; String[][] social=StatFactory.MakeParam(truth, null); r.AddStatString(social);
             return r;
@@ -62,27 +68,22 @@ public class Ability_List_Summon
             default: return null; 
         }
     }
-    public static Ability MakeAbRinger (int counter)
+    private static Ability MakeAbRinger (int counter)
     {
         switch (counter)
         {
-            case 0: BasicAb rend= new BasicAb("Ring Shot", "single", "enemy", 35);
-            return rend;
-            case 1: AttackAb e= new AttackAb("Explosive Rings", "single", "enemy", 30, 2, 1);
-            return e;
-            case 3: AttackAb c= new AttackAb("Constrictor Rings", "single", "enemy", 80, 4);
-            return c;
+            case 0: return new BasicAb("Ring Shot", "single", "enemy", 35);
+            case 1: return new AttackAb("Explosive Rings", "single", "enemy", 30, 2, 1);
+            case 3: return new AttackAb("Constrictor Rings", "single", "enemy", 80, 4);
             default: return null; 
         }
     }
-    public static Ability MakeAbBruin (int counter)
+    private static Ability MakeAbBruin (int counter)
     {
         switch (counter)
         {
-            case 0: BasicAb rend= new BasicAb("Body Slam", "single", "enemy", 35);
-            return rend;
-            case 1: BasicAb r= new BasicAb("Lacerate", "single", "enemy", 60, 2);
-            return r;
+            case 0: return new BasicAb("Body Slam", "single", "enemy", 35);
+            case 1: return new BasicAb("Lacerate", "single", "enemy", 60, 2);
             case 3: AttackAb p= new AttackAb("Bellow", "single", "enemy", 70, 4); p.together=true;
             String[] i={"Provoke", "100", "616", "1", "false"}; String[][] z=StatFactory.MakeParam(i, null); p.AddStatString(z);
             String[] truth={"Resistance", "100", "15", "1", "true"}; String[][] social=StatFactory.MakeParam(truth, null); p.AddStatString(social);
@@ -90,28 +91,26 @@ public class Ability_List_Summon
             default: return null; 
         }
     }
-    public static Ability MakeAbGiganto (int counter)
+    private static Ability MakeAbGiganto (int counter)
     {
         switch (counter)
         {
-            case 0: BasicAb rend= new BasicAb("Chomp", "single", "enemy", 60);
-            return rend;
+            case 0: return new BasicAb("Chomp", "single", "enemy", 60);
             default: return null; 
         }
     }
-    public static Ability MakeAbDaemon (int counter)
+    private static Ability MakeAbDaemon (int counter)
     {
         switch (counter)
         {
             case 0: BasicAb rend= new BasicAb("Unnerving Strike", "single", "enemy", 25); 
             String[] carrot={"Provoke", "100", "616", "1", "false"}; String[][] garrote=StatFactory.MakeParam(carrot, null); rend.AddStatString(garrote);
             return rend;
-            case 1: BasicAb blast= new BasicAb("Rend", "single", "enemy", 35); 
-            return blast;
+            case 1: return new BasicAb("Rend", "single", "enemy", 35); 
             default: return null; 
         }
     }
-    public static Ability MakeAbLilDoomie (int counter)
+    private static Ability MakeAbLilDoomie (int counter)
     {
         switch (counter)
         {
@@ -122,7 +121,7 @@ public class Ability_List_Summon
             default: return null; 
         }
     }
-    public static Ability MakeAbDrone (int counter)
+    private static Ability MakeAbDrone (int counter)
     {
         switch (counter)
         {
@@ -135,29 +134,27 @@ public class Ability_List_Summon
             default: return null; 
         }
     }
-    public static Ability MakeAbCrushbot (int counter)
+    private static Ability MakeAbCrushbot (int counter)
     {
         switch (counter)
         {
-            case 0: BasicAb d= new BasicAb("Crush", "single", "enemy", 35);
-            return d;
-            case 1: BasicAb c= new BasicAb("Destroy", "lowest", "enemy", 25);
-            return c;
+            case 0: return new BasicAb("Crush", "single", "enemy", 35);
+            case 1: return new BasicAb("Destroy", "lowest", "enemy", 25);
             default: return null; 
         }
     }
-    public static Ability MakeAbRTrooper (int counter)
+    private static Ability MakeAbRTrooper (int counter)
     {
         switch (counter)
         {
-            case 0: BasicAb rpg= new BasicAb("RPG", "single", "enemy", 30); rpg.special.add(new Ricochet(500));
-            return rpg;
-            case 1: BasicAb aoe= new BasicAb("Rocket Barrage", "AoE", "enemy", 20); 
-            return aoe;
+            case 0: //BasicAb rpg= new BasicAb("RPG", "single", "enemy", 30); rpg.special.add(new Ricochet(500));
+            //return rpg;
+            return new BasicAb("RPG", "single", "enemy", 30).special.add(new Ricochet(500));
+            case 1: return new BasicAb("Rocket Barrage", "AoE", "enemy", 20); 
             default: return null; 
         }
     }
-    public static Ability MakeAbNaught (int counter)
+    private static Ability MakeAbNaught (int counter)
     {
         switch (counter)
         {
@@ -174,7 +171,7 @@ public class Ability_List_Summon
             default: return null; 
         }
     }
-    public static Ability MakeAbSpiderling (int counter)
+    private static Ability MakeAbSpiderling (int counter)
     {
         switch (counter)
         {
@@ -193,14 +190,14 @@ public class Ability_List_Summon
             default: return null; 
         }
     }
-    public static Ability MakeAbNickLMD (int counter)
+    private static Ability MakeAbNickLMD (int counter)
     { 
         switch (counter)
         {
-            case 0: BasicAb pistol= new BasicAb ("Concealed Pistol", "single", "enemy", 35); return pistol;
+            case 0: return new BasicAb ("Concealed Pistol", "single", "enemy", 35);
             case 1: BuffAb cache= new BuffAb ("Hidden Weapons Cache", "single", "ally inclusive", 0); 
-            String[] sword= {"Intensify", "100", "15", "1", "knull"}; 
-            String[][] redo=StatFactory.MakeParam(sword, null); cache.AddStatString(redo);
+            String[] sword= {"Intensify", "100", "15", "1", "knull"}; String[][] redo=StatFactory.MakeParam(sword, null); 
+            cache.AddStatString(redo);
             return cache;
             default: return null;
         }
